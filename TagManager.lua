f8fff6dd0de1c3d845893f03c16aaa94f84efac0
Qiler <Qilerx@gmail.com>
2018-09-18 22:38:40 +0200

    Added engine tag support. +fixed errors

--- TagManager.lua
+++ TagManager.lua
@@ -1,6 +1,6 @@
 --[[
     Shadow Templar Tag Manager
-    Version 0.00000000001
+    Version 1.02
 	
 	usage:
 		exampleGroup = TagManager("all,vertical")
@@ -11,18 +11,29 @@
 
 function TagManager(tagString)
 	self = {}
-	self.tagArray = {}
-	self.tags = ""
+	local tagArray = {}
+	local tags = ""
+
+	local function explode(div,str)
+		if (div=='') then return false end
+		local pos,arr = 0,{}
+		for st,sp in function() return string.find(str,div,pos,true) end do
+		  table.insert(arr,string.sub(str,pos,st-1))
+		  pos = sp + 1
+		end
+		table.insert(arr,string.sub(str,pos))
+		return arr
+	end
 	
 	function self.tagsToString()
-		self.tags = table.concat(self.tagArray,",")
+		tags = table.concat(tagArray,",")
 	end
 	
 	function self.Remove(s)
 		if type(s) ~= "string" then error("[TagManager] Unable to remove a tag - Not a string") end
-		for k,v in pairs(self.tagArray) do
+		for k,v in pairs(tagArray) do
 			if s == v then
-				table.remove(self.tagArray,k)
+				table.remove(tagArray,k)
 			end
 		end
 		self.tagsToString()
@@ -30,12 +41,12 @@ function TagManager(tagString)
 	
 	function self.Add(s)
 		if type(s) ~= "string" then error("[TagManager] Unable to add a tag - Not a string") end
-		for k,v in pairs(self.tagArray) do  --Do we even need to make sure its not already in the table?
+		for k,v in pairs(tagArray) do
 			if s == v then
 				return
 			end
 		end
-		table.insert(self.tagArray,s)
+		table.insert(tagArray,s)
 		self.tagsToString()
 	end
 	
@@ -43,18 +54,6 @@ function TagManager(tagString)
 		self.Add(v)
 	end
 	
-	setmetatable (self, { __tostring = function (self) return self.tags end })
+	setmetatable (self, { __tostring = function (self) return tags end })
 	return self
-end
-
---utils
-function explode(div,str)
-  if (div=='') then return false end
-  local pos,arr = 0,{}
-  for st,sp in function() return string.find(str,div,pos,true) end do
-    table.insert(arr,string.sub(str,pos,st-1))
-    pos = sp + 1
-  end
-  table.insert(arr,string.sub(str,pos))
-  return arr
 end
