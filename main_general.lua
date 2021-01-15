7685dd8a6df987f0fa72ab1f02ed3c68ea7b8f80
sysadrift <sysadrift@internal.shadowtemplar.org>
2021-01-15 00:03:38 -0500

    Moved main_general.lua to top level directory.

---
+++ main_general.lua
@@ -0,0 +1,55 @@
+--@require EventDelegate
+--@require TaskManager
+--@require DynamicDocument
+--@require CSS_SHUD
+--@require SimpleSlotDetector
+--@require TagManager
+--@require KeybindController
+--@require STEC
+--@require SHUD
+--@require MouseMovement
+--@require STEC_Config
+--@timer SHUDRender
+--@class Main
+
+_G.BuildUnit = {}
+local Unit = _G.BuildUnit
+_G.BuildSystem = {}
+local System = _G.BuildSystem
+
+function Unit.Start()
+	Events.Flush.Add(mouse.apply)
+	Events.Flush.Add(ship.apply)
+	Events.Update.Add(SHUD.Update)
+	
+	system.print([[Horizon 1.1.1.5]])
+end
+
+function Unit.Stop()
+	system.showScreen(0)
+end
+
+function Unit.Tick(timer)
+	if timer == "SHUDRender" then
+		if SHUD then SHUD.Render() end
+	end
+end
+
+function System.ActionStart(action)
+	keybindPresets[keybindPreset].Call(action, "down")
+end
+
+function System.ActionStop(action)
+	keybindPresets[keybindPreset].Call(action, "up")
+end
+
+function System.ActionLoop(action)
+end
+
+function System.Update()
+	if Events then Events.Update() end
+end
+
+function System.Flush()
+	if Events then Events.Flush() end
+end
